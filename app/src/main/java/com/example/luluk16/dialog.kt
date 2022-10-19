02c0e975627e11e64d8bca1a05f450fb12076af6
luluk05 <luluk4696@gmail.com>
2022-10-19 08:39:29 +0700

    first commit

---
+++ app/src/main/java/com/example/luluk16/dialog.kt
@@ -0,0 +1,15 @@
+package com.example.luluk16
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_dialog)
+    }
+}
